5b98be1b4d361132fa8ccb3c5ab9817b35f8543c
Jeff Tratner <jeffrey.tratner@gmail.com>
2021-05-11 23:52:37 -0700

    Blackify cleanups

--- settings_test.py
+++ settings_test.py
@@ -1,19 +1,19 @@
 from pathlib import Path
+
 ROOT_DIR = Path(__file__).parent
-DEBUG=True
-DATABASES={
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+DEBUG = True
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
     }
 }
-SECRET_KEY="whatever"
+SECRET_KEY = "whatever"
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'turtle_shell',
-    'graphene_django',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "turtle_shell",
+    "graphene_django",
 )
-
